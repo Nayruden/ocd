90b93b27f84e62094d8cf7eb743f253311e23c10
Nayruden <nayruden@gmail.com>
2011-01-29 10:37:53 -0600

    * [ADD] Sort command. * [ADD] All commands will now combine stacks as much as possible. * [FIX] Putting items in the wrong starting row with large chests.

--- README.txt
+++ README.txt
@@ -1,20 +1,29 @@
-OChestDump v0.6
+OChestDump v0.7
 Copyright (c) 2011 Nayruden <http://omnivr.net>
 
 OChestDump allows you to dump the contents of your inventory to a chest, or to
 dump the contents of a chest into your inventory, or to swap the contents of a
-chest with your inventory.
+chest with your inventory, or to sort a chest.
 
-To use, type "/ocd swap", "/ocd loot", or "/ocd stash" in chat while looking at
-a chest.
+To use, type "/ocd swap", "/ocd loot", "/ocd stash", or "/ocd sort" in chat
+while looking at a chest. Typing "/ocd" in chat will bring up the help for the
+command.
 
 Loot fits as many items from the chest into your inventory as possible.
 Stash fits as many items from your inventory into the chest as possible.
 Swap simply swaps the items from the chest and your inventory as best as it can.
 You can add an item name or ID after swap or loot to exchange only the specified
 item.
+Sort allows you to sort a chest by name (default) or total amount of items.
+
+All commands combine stacks where possible to save space!
 
 Changelog:
+v0.7 - 1/29/2011
+    * [ADD] Sort command.
+    * [ADD] All commands will now combine stacks as much as possible.
+    * [FIX] Putting items in the wrong starting row with large chests.
+
 v0.6 - 1/27/2011
     * [ADD] Ability to stash and loot only a specific item.
 
--- src/net/omnivr/ocd/OCDPlayerListener.java
+++ src/net/omnivr/ocd/OCDPlayerListener.java
@@ -1,8 +1,13 @@
 package net.omnivr.ocd;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
 import net.omnivr.olib.Constants;
 import net.omnivr.olib.ItemDB;
 import net.omnivr.olib.Trace;
+import net.omnivr.olib.Util;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -31,22 +36,15 @@ public class OCDPlayerListener extends PlayerListener {
         Player player = event.getPlayer();
 
         if (pieces[0].equalsIgnoreCase("/ocd")) {
-            if (pieces.length < 2 || pieces.length > 3 || !(pieces[1].equalsIgnoreCase("stash") || pieces[1].equalsIgnoreCase("loot") || pieces[1].equalsIgnoreCase("swap"))) {
+            if (pieces.length < 2 || pieces.length > 3 || !(pieces[1].equalsIgnoreCase("stash") || pieces[1].equalsIgnoreCase("loot") || pieces[1].equalsIgnoreCase("swap") || pieces[1].equalsIgnoreCase("sort"))) {
                 player.sendMessage(ChatColor.RED + "/ocd <stash|loot|swap> [item]");
+                player.sendMessage(ChatColor.RED + "OR /ocd sort [<name|amount>]");
                 player.sendMessage(ChatColor.RED + "stash - Put all items in inventory in the chest you're looking at");
                 player.sendMessage(ChatColor.RED + "loot - Put all items in the chest into your inventory");
                 player.sendMessage(ChatColor.RED + "swap - Exhange items between your inventory and chest");
+                player.sendMessage(ChatColor.RED + "sort - Sort items by name or amount, defaults to name");
                 player.sendMessage(ChatColor.RED + "Adding item ID/name will exchange only that item for stash/loot");
                 return;
-            }
-
-            int item_id = 0;
-            if (pieces.length == 3) {
-                item_id = ItemDB.nameOrIDToID(pieces[2]);
-                if (item_id == -1) {
-                    player.sendMessage(ChatColor.RED + "Unknown item name/id: " + pieces[2]);
-                    return;
-                }
             }
 
             Block block = Trace.Simple(player, 4.0); // 4m max
@@ -61,33 +59,69 @@ public class OCDPlayerListener extends PlayerListener {
             ItemStack[] chest2_contents = null;
             for (BlockFace neighbor : Constants.NEIGHBORS) {
                 if (block.getRelative(neighbor).getType() == Material.CHEST) {
-                    chest2 = (ContainerBlock) block.getRelative(neighbor).getState();
-                    chest2_contents = chest2.getInventory().getContents();
+                    ContainerBlock neighbor_chest = (ContainerBlock) block.getRelative(neighbor).getState();
+                    ItemStack[] neighbor_chest_contents = neighbor_chest.getInventory().getContents();
+                    if (neighbor == BlockFace.NORTH || neighbor == BlockFace.EAST) {
+                        chest2 = chest1;
+                        chest2_contents = chest1_contents;
+                        chest1 = neighbor_chest;
+                        chest1_contents = neighbor_chest_contents;
+                    } else {
+                        chest2 = neighbor_chest;
+                        chest2_contents = neighbor_chest_contents;
+                    }
                     break;
                 }
             }
 
+            ItemStack[] chest_contents = chest1_contents;
+            if (chest2 != null) {
+                chest_contents = Util.concat(chest1_contents, chest2_contents);
+            }
+
             ItemStack[] player_contents = player.getInventory().getContents();
 
-            if (pieces[1].equalsIgnoreCase("stash")) {
-                tryFill(player_contents, chest1_contents, item_id);
-                tryFill(player_contents, chest2_contents, item_id);
-            } else if (pieces[1].equalsIgnoreCase("loot")) {
-                tryFill(chest1_contents, player_contents, item_id);
-                tryFill(chest2_contents, player_contents, item_id);
-            } else if (chest2 == null) {
-                ItemStack[] new_chest1_contents = new ItemStack[chest1_contents.length];
-                tryFill(player_contents, new_chest1_contents, 0);
-                tryFill(chest1_contents, player_contents, 0);
-                chest1_contents = new_chest1_contents;
-            } else {
-                ItemStack[] new_player_contents = new ItemStack[player_contents.length];
-                tryFill(chest1_contents, new_player_contents, 0);
-                tryFill(chest2_contents, new_player_contents, 0);
-                tryFill(player_contents, chest1_contents, 0);
-                tryFill(player_contents, chest2_contents, 0);
-                player_contents = new_player_contents;
+            if (pieces[1].equalsIgnoreCase("sort")) {
+                compactInventory(chest_contents);
+                if (pieces.length == 2 || pieces[2].equalsIgnoreCase("name")) {
+                    Arrays.sort(chest_contents, new orderByName());
+                } else if (pieces[2].equalsIgnoreCase("amount")) {
+                    Arrays.sort(chest_contents, new orderByAmount(chest_contents));
+                } else {
+                    player.sendMessage(ChatColor.RED + "Unknown sort: " + pieces[2]);
+                    return;
+                }
+            } else { // loot, stash, swap
+
+                int item_id = 0;
+                if (pieces.length == 3) {
+                    item_id = ItemDB.nameOrIDToID(pieces[2]);
+                    if (item_id == -1) {
+                        player.sendMessage(ChatColor.RED + "Unknown item name/id: " + pieces[2]);
+                        return;
+                    }
+                }
+
+                if (pieces[1].equalsIgnoreCase("stash")) {
+                    tryFill(player_contents, chest_contents, item_id);
+                } else if (pieces[1].equalsIgnoreCase("loot")) {
+                    tryFill(chest_contents, player_contents, item_id);
+                } else {
+                    ItemStack[] new_chest_contents = new ItemStack[chest_contents.length];
+                    tryFill(player_contents, new_chest_contents, 0);
+                    tryFill(chest_contents, player_contents, 0);
+                    tryFill(chest_contents, new_chest_contents, 0);
+                    chest_contents = new_chest_contents;
+                }
             }
+
+            if (chest2 == null) {
+                chest1_contents = chest_contents;
+            } else {
+                System.arraycopy(chest_contents, 0, chest1_contents, 0, chest1_contents.length);
+                System.arraycopy(chest_contents, chest1_contents.length, chest2_contents, 0, chest2_contents.length);
+            }
+
             chest1.getInventory().setContents(chest1_contents);
             if (chest2 != null) {
                 chest2.getInventory().setContents(chest2_contents);
@@ -100,27 +134,82 @@ public class OCDPlayerListener extends PlayerListener {
     }
 
     private void tryFill(ItemStack[] inventory_from, ItemStack[] inventory_to, int restricted_item_id) {
-        if (inventory_from == null || inventory_to == null) {
-            return;
-        }
-
         int from_size = inventory_from.length;
         int to_size = inventory_to.length;
-        int from_slot = 0;
-        for (int to_slot = 0; to_slot < to_size; to_slot++) {
-            if (inventory_to[to_slot] != null && inventory_to[to_slot].getAmount() != 0) {
+        for (int from_slot = 0; from_slot < from_size; from_slot++) {
+            if (inventory_from[from_slot] == null) {
                 continue;
             }
 
-            for (; from_slot < from_size && (inventory_from[from_slot] == null || inventory_from[from_slot].getAmount() == 0
-                    || (restricted_item_id != 0 && inventory_from[from_slot].getTypeId() != restricted_item_id)); from_slot++);
-            if (from_slot >= from_size) {
-                break;
+            ItemStack from_stack = inventory_from[from_slot];
+            if (from_stack.getAmount() == 0 || (restricted_item_id != 0 && from_stack.getTypeId() != restricted_item_id)) {
+                continue;
             }
 
-            inventory_to[to_slot] = inventory_from[from_slot];
-            inventory_from[from_slot] = null;
-            from_slot++;
+            for (int to_slot = 0; to_slot < to_size; to_slot++) {
+                ItemStack to_stack = inventory_to[to_slot];
+                if (to_stack == null || to_stack.getAmount() == 0) {
+                    inventory_to[to_slot] = from_stack;
+                    inventory_from[from_slot] = null;
+                    break;
+                }
+                compactStack(from_stack, to_stack); // Compact if possible
+            }
+        }
+    }
+
+    private void compactStack(ItemStack from_stack, ItemStack to_stack) {
+        if (from_stack.getType() == to_stack.getType()) {
+            int max = from_stack.getType().getMaxStackSize();
+            int diff = Math.min(from_stack.getAmount() + to_stack.getAmount(), max) - to_stack.getAmount();
+            to_stack.setAmount(to_stack.getAmount() + diff);
+            from_stack.setAmount(from_stack.getAmount() - diff);
+        }
+    }
+
+    private void compactInventory(ItemStack[] stacks) {
+        for (int from = 1; from < stacks.length; from++) {
+            for (int to = 0; to < from; to++) {
+                compactStack(stacks[from], stacks[to]);
+            }
+        }
+    }
+
+    private class orderByName implements Comparator<ItemStack> {
+
+        public int compare(ItemStack a, ItemStack b) {
+            if (a == null || a.getAmount() == 0) {
+                return 1;
+            }
+            if (b == null || b.getAmount() == 0) {
+                return -1;
+            }
+            return a.getType().toString().compareToIgnoreCase(b.getType().toString());
+        }
+    }
+
+    private class orderByAmount implements Comparator<ItemStack> {
+
+        Map<Integer, Integer> amounts = new TreeMap<Integer, Integer>();
+
+        public orderByAmount(ItemStack[] stacks) {
+            for (ItemStack stack : stacks) {
+                Integer amount = amounts.get(stack.getTypeId());
+                if (amount == null) {
+                    amount = Integer.valueOf(0);
+                }
+                amounts.put(stack.getTypeId(), amount + stack.getAmount());
+            }
+        }
+
+        public int compare(ItemStack a, ItemStack b) {
+            if (a == null || a.getAmount() == 0) {
+                return 1;
+            }
+            if (b == null || b.getAmount() == 0) {
+                return -1;
+            }
+            return amounts.get(b.getTypeId()) - amounts.get(a.getTypeId());
         }
     }
 }
